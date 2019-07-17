e960557e33750b31a33934c9a7381ff0b7b36dd1
Petr Stoklas <petrstoklas@gmail.com>
2019-07-17 16:05:06 +0200

    Wheel zoom reversed.

--- src/Scroller.js
+++ src/Scroller.js
@@ -635,7 +635,7 @@ var members = {
   doMouseZoom: function (wheelDelta, timeStamp, pageX, pageY) {
 
     var self = this;
-    var change = wheelDelta > 0 ? 0.97 : 1.03;
+    var change = wheelDelta < 0 ? 0.97 : 1.03;
 
     return self.zoomTo(self.__zoomLevel * change, false, pageX - self.__clientLeft, pageY - self.__clientTop);
 
